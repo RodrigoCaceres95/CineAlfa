716ee71c7b54394f70fe727818001bfba779571c
Rodrigo Cáceres <rodri.caceres.95@gmail.com>
2022-03-22 17:23:00 -0300

    practica de funciones

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 //loop
-let pregunta = prompt("Te gusta el cine?")
+// let pregunta = prompt("Te gusta el cine?")
+// if (String (pregunta.toUpperCase()) == "SI"){
+//     alert("Entraste al lugar indicado")
+// } else {
+//     alert("Este sitio no es para ti")
+// }
+// let respuesta = prompt("Que pelicula es tu favorita?")
+// while (respuesta.toUpperCase() !="ESC") {
+//     alert("Te gusta " + respuesta)
+//     respuesta = prompt ("Ingresa otra pelicula que te guste...")
+// }
 
-if (String (pregunta.toUpperCase()) == "SI"){
-    alert("Entraste al lugar indicado")
-} else {
-    alert("Este sitio no es para ti")
+//let respuesta = prompt("Que pelicula es tu favorita?")
+
+//funciones con loop interactivo
+function pregunta(respuesta) {
+    switch (respuesta.toLowerCase()) {
+        case "si": 
+            return "Entraste al lugar indicado";
+            break;
+        case "obvio":
+            return "Entraste al lugar indicado";
+            break;
+        default: 
+            return "Esta pagina no es para vos";
+            break;
 }
-let respuesta = prompt("Que pelicula es tu favorita?")
-while (respuesta.toUpperCase() !="ESC") {
-    alert("Te gusta " + respuesta)
-    respuesta = prompt ("Ingresa otra pelicula que te guste...")
 }
+let teGustaElCine = prompt("Te gusta el cine?");
+alert(pregunta(teGustaElCine));
+
+function promptInteractivo(cine) {
+    while (cine.toUpperCase() !="esc") {
+        alert("Te gusta " + cine);
+        cine = prompt ("Ingresa otra pelicula que te guste...");
+}
+}
+let teGustanLosPrompts = prompt("Que pelicula es tu favorita?");
+alert(promptInteractivo(teGustanLosPrompts));
