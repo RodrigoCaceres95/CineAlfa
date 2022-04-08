45afdff65cbce4e10ed9dd1fa62e1b98a5874f9d
Rodrigo Cáceres <rodri.caceres.95@gmail.com>
2022-04-07 21:19:16 -0300

    eventos

---
+++ app_pagina4.js
@@ -0,0 +1,11 @@
+var Capturar = function(event){
+    let datosCliente = document.getElementsByClassName("input"), guardarDatos = [];
+    for (var i = 0; i <= datosCliente.length; i++){
+        guardarDatos[i] = datosCliente[i];
+        console.log(datosCliente[i].value)
+        event.preventDefault()
+    }
+
+}
+let boton = document.getElementById("btn");
+boton.addEventListener("click", Capturar)
