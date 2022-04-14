a1fae1dcfbc16b469643a6ee58b4e6d9ea9afbbf
Rodrigo Cáceres <rodri.caceres.95@gmail.com>
2022-04-14 03:01:34 -0300

    guardar datos de un cliente en Storage y en formato JSON

--- app_pagina4.js
+++ app_pagina4.js
@@ -3,9 +3,11 @@ var Capturar = function(event){
     for (var i = 0; i <= datosCliente.length; i++){
         guardarDatos[i] = datosCliente[i];
         console.log(datosCliente[i].value)
+        localStorage.setItem(datosCliente[i].id, JSON.stringify(datosCliente[i].value))
         event.preventDefault()
     }
 
 }
+
 let boton = document.getElementById("btn");
 boton.addEventListener("click", Capturar)
