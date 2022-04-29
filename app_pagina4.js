6f8ec9c731ff154ed239c12521ac11c48e2c2ad8
Rodrigo Cáceres <rodri.caceres.95@gmail.com>
2022-04-29 05:32:30 -0300

    fetch y codigo mejorado

--- app_pagina4.js
+++ app_pagina4.js
@@ -1,3 +1,58 @@
+let tituloFecha = document.getElementById("fecha")
+const dia = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']
+const fecha = new Date()
+function fechaActual() {
+    dia[fecha.getDay()] === 'Miércoles','Viernes' ? tituloFecha.innerHTML = `<h1 class="h1">Hoy es ` + dia[fecha.getDay()] + ", hoy todas las entradas salen $300</h1>" : tituloFecha.innerHTML = `<h1 class="h1">Hoy es ` + dia[fecha.getDay()] + ", hoy las entradas 3D salen $500 y las 2D salen $400</h1>";
+}
+function cobrarEntradas(){
+    let valorEntrada = document.miFormulario.sala.value
+    let entradasTotales = document.miFormulario.cantEntradas.value
+    let peliculaSeleccionada = document.miFormulario.pelicula.value
+    if(dia[fecha.getDay()] === 'Miércoles', 'Viernes') {
+        let totalEntradas = 300*entradasTotales;
+        if (valorEntrada === "2D") {
+           // alert(`Has seleccionado la sala ${valorEntrada}, reservaste una cantidad de ${entradasTotales} entrada/s y como estamos en ${dia[fecha.getDay()]}, tus entradas cuestan $300 cada una, total a pagar: AR$${totalEntradas}`)
+           Swal.fire({
+            icon: "success",
+            title: "Reserva exitosa",
+            text: `Pronto te enviaremos un link de pago a tu e-mail. Has seleccionado la película ${peliculaSeleccionada}, en la sala ${valorEntrada}, reservaste una cantidad de ${entradasTotales} entrada/s y como estamos en ${dia[fecha.getDay()]}, tus entradas cuestan $300 cada una, total a pagar: AR$${totalEntradas}`,
+            confirmButtonText: "OK",
+        })
+        return(true)
+        } else {
+            //alert(`Has seleccionado la sala ${valorEntrada}, reservaste una cantidad de ${entradasTotales} entrada/s y como estamos en ${dia[fecha.getDay()]}, tus entradas cuestan $300 cada una, total a pagar: AR$${totalEntradas}`)
+            Swal.fire({
+                icon: "success",
+                title: "Reserva exitosa",
+                text: `Pronto te enviaremos un link de pago a tu e-mail. Has seleccionado la película ${peliculaSeleccionada}, en la sala ${valorEntrada}, reservaste una cantidad de ${entradasTotales} entrada/s y como estamos en ${dia[fecha.getDay()]}, tus entradas cuestan $300 cada una, total a pagar: AR$${totalEntradas}`,
+                confirmButtonText: "OK",
+            })
+            return(true)
+        }
+    } else {
+        if (valorEntrada === "2D"){
+            totalEntradas = 400*entradasTotales;
+            //alert(`Has seleccionado la sala ${valorEntrada}, reservaste una cantidad de ${entradasTotales} entrada/s y como estamos en ${dia[fecha.getDay()]}, tus entradas cuestan $400 cada una, total a pagar: AR$${totalEntradas}`)
+            Swal.fire({
+                icon: "success",
+                title: "Reserva exitosa",
+                text: `Pronto te enviaremos un link de pago a tu e-mail. Has seleccionado la película ${peliculaSeleccionada}, en la sala ${valorEntrada}, reservaste una cantidad de ${entradasTotales} entrada/s y como estamos en ${dia[fecha.getDay()]}, tus entradas cuestan $300 cada una, total a pagar: AR$${totalEntradas}`,
+                confirmButtonText: "OK",
+            })
+            return(true)
+        } else {
+            totalEntradas = 500*entradasTotales;
+            //alert(`Has seleccionado la sala ${valorEntrada}, reservaste una cantidad de ${entradasTotales} entrada/s y como estamos en ${dia[fecha.getDay()]}, tus entradas cuestan $500 cada una, total a pagar: AR$${totalEntradas}`)
+            Swal.fire({
+                icon: "success",
+                title: "Reserva exitosa",
+                text: `Pronto te enviaremos un link de pago a tu e-mail. Has seleccionado la película ${peliculaSeleccionada}, en la sala ${valorEntrada}, reservaste una cantidad de ${entradasTotales} entrada/s y como estamos en ${dia[fecha.getDay()]}, tus entradas cuestan $300 cada una, total a pagar: AR$${totalEntradas}`,
+                confirmButtonText: "OK",
+            })
+        return(true)
+        }
+    }
+}
 function validar(){
     if (document.miFormulario.Nombre.value === "") {
         Swal.fire({
@@ -49,25 +104,44 @@ function validar(){
         })
         return false;
     }
-    Swal.fire({
-        icon: "success",
-        title: "Reserva exitosa",
-        text: "Enseguida te enviaremos un mail con el link de pago",
-        confirmButtonText: "OK",
-    })
+    if (document.miFormulario.sala.value === "No seleccionaste tu sala") {
+        // alert("Te ha faltado elegir tu película!")
+        Swal.fire({
+            icon: "error",
+            title: "Operación incompleta",
+            text: "Te ha faltado elegir tu sala!",
+            confirmButtonText: "OK",
+        })
+        return false;
+    }
+    fetch("https://formsubmit.co/ajax/rodri.caceres.95@gmail.com", {
+            method: "POST",
+            body: new FormData(validar.target)
+        })
+            .then(res=>res.ok ? res.json(): Promise.reject(res))
+            .then(json=>{
+                console.log(json)
+            })
+            .catch(err=>{
+                console.log(err)
+            })
     return(true)
 }
+
 var Capturar = function(event){
     let datosCliente = document.getElementsByClassName("input"), guardarDatos = [];
     for (var i = 0; i <= datosCliente.length; i++){
         guardarDatos[i] = datosCliente[i];
         localStorage.setItem(datosCliente[i].id, JSON.stringify(datosCliente[i].value))
-        datosCliente[i].value === "" ? console.log("Debes completar datos!") : console.log(datosCliente[i].value)
+        datosCliente[i].value === "" ? console.log("Dato no ingresado") : console.log(datosCliente[i].value)
         event.preventDefault()
+        
     }
-
+    
 }
-
 let boton = document.getElementById("btn");
+boton.addEventListener("click", cobrarEntradas)
 boton.addEventListener("click", validar)
 boton.addEventListener("click", Capturar)
+
+fechaActual()
